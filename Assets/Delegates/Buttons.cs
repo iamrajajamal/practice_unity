9fa9650ed01b6377d77f87692e92117d2248fcd8
iamrajajamal <rajajamalparvez@gmail.com>
2019-07-18 14:35:16 +0500

    Delegate for UI

---
+++ Assets/Delegates/Buttons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Buttons : MonoBehaviour, IPointerClickHandler
+{
+    public UIButtonManager.MyButtons MyButtons_enum;
+
+    public delegate void ButtonPressed(UIButtonManager.MyButtons myButtons);
+
+    public static event ButtonPressed buttonPressed_event;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (buttonPressed_event != null)
+            buttonPressed_event(MyButtons_enum);
+    }
+}
---
+++ Assets/Delegates/UIButtonManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButtonManager : MonoBehaviour
+{
+
+    public Text changeText;
+
+    private void OnEnable()
+    {
+        Buttons.buttonPressed_event += ONButtonPressed;
+    }
+
+    private void OnDisable()
+    {
+        Buttons.buttonPressed_event -= ONButtonPressed;
+    }
+
+    private void ONButtonPressed(MyButtons myButtons)
+    {
+        switch (myButtons)
+        {
+            case MyButtons.Leaderboard:
+                changeText.text = "Leaderboard Pressed";
+                break;
+
+            case MyButtons.Market:
+                changeText.text = "Market Pressed";
+                break;
+
+            case MyButtons.Achievement:
+                changeText.text = "Achievement Pressed";
+                break;
+
+            case MyButtons.Play:
+                changeText.text = "Play Pressed";
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public enum MyButtons
+    {
+        Leaderboard,
+        Market,
+        Achievement,
+        Play
+    }
+}
